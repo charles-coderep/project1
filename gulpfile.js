dcfa34efb8d45a92aadce84e1548e6e184876ecc
charles-coderep <75278224+charles-coderep@users.noreply.github.com>
2020-12-09 02:45:27 +0000

    adding more to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,10 @@ const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
 //compile scss into css
 
-task('style', function() {
+function style() {
     return gulp.src('src/scss/**/*.scss')
     .pipe(sass().on('error',sass.logError))
     .pipe(gulp.dest('src/css'))
     .pipe(browserSync.stream());
-});
+}
 exports.style = style;
