60648606de87084344c966d96385d0e48ebd74c9
charles-coderep <75278224+charles-coderep@users.noreply.github.com>
2021-01-03 21:47:03 +0000

    first commit in dec

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,23 @@
-const gulp = require("gulp");
+const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
 //compile scss into css
-
 function style() {
     return gulp.src('src/scss/**/*.scss')
     .pipe(sass().on('error',sass.logError))
     .pipe(gulp.dest('src/css'))
-    .pipe(browserSync.stream());
+    .pipe(browserSync.stream()); // stream to browser
+}
+function watch() {
+    browserSync.init({
+        server: {
+           baseDir: "./src",
+           index: "/index.html"
+        }
+    });
+    gulp.watch('src/scss/**/*.scss', style).on('change', browserSync.reload); // watch changes to scss, if any go to style task and convert to css
+    gulp.watch('./*.html').on('change',browserSync.reload);
+    gulp.watch('./js/**/*.js').on('change', browserSync.reload);
 }
 exports.style = style;
+exports.watch = watch;
