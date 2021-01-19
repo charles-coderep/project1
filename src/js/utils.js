863717e23685336677074c4b276c566158033581
charles-coderep <75278224+charles-coderep@users.noreply.github.com>
2021-01-19 01:06:05 +0000

    added js css manipulation

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,3 +1,21 @@
 function showMessage (message) {
     document.getElementById('messageid').textContent = message;
     }
+
+    let buttonOne = document.querySelector("a.btn.one");
+    let card = document.querySelector("section.card");
+
+    buttonOne.addEventListener("click",function(){
+        card.classList.toggle('addstyle');
+    });
+
+    let boxes = document.querySelectorAll("section div");
+
+    let buttonTwo = document.querySelector("a.btn.two");
+    buttonTwo.addEventListener("click", function(){
+        console.log(boxes);
+        for(let i = 0; 1<boxes.length;i++){
+            boxes[i].style.backgroundColor = "#fa4";
+        }
+    });
+    
